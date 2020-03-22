7ba513b0bcb7ec2e28576b370e60a49a1fdf3b5e
cchay <cchayanin@gmail.com>
2020-03-22 23:47:27 +0700

    exercise1 & exercise2

---
+++ 5.Javascript/exercise1/exercise1.js
@@ -0,0 +1,5 @@
+let name = "Codecamp";
+
+console.log(`hello ${1}`);
+console.log(`hello ${"name"}`);
+console.log(`hello ${name}`);
---
+++ 5.Javascript/exercise2/exercise2.js
@@ -0,0 +1,6 @@
+let name = "Ice";
+let age = 34;
+let address = "Khonkaen";
+let profile = `My name is ${name}, I'm ${age} years old and I live in ${address}.`;
+
+console.log(profile);
