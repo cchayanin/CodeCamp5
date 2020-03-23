739a23c76574462b26698e4efd0bee4e23a9ca15
cchay <cchayanin@gmail.com>
2020-03-23 15:05:25 +0700

    exercise 3-5

---
+++ 5.Javascript/exercise3/exercise3.2.js
@@ -0,0 +1,17 @@
+// 2. จงหาผลลัพธ์ของ Statement ต่อไปนี้
+
+console.log("" + 1 + 0); //10 string
+console.log("" - 1 + 0); //-1 number
+console.log(true + false); //1 number
+console.log(6 / "3"); //2 number
+console.log("2" * "3"); //6 number
+console.log(4 + 5 + "px"); //9px string
+console.log("$" + 4 + 5); //$45 string
+console.log("4" - 2); //2 number
+console.log("4px" - 2); //Nan
+console.log(7 / 0); //Infinity
+console.log("  -9  " + 5); //  -9  5 string
+console.log("  -9  " - 5); //-14 number
+console.log(null + 1); //1 number
+console.log(undefined + 1); //Nan
+console.log(" \t \n" - 2); //-2 number
---
+++ 5.Javascript/exercise5/exercise5.3.js
@@ -0,0 +1,30 @@
+// 3. ใช้ prompt ในการรับคะแนนมาคำนวณเกรด
+//      ถ้าคะแนน มากกว่าเท่ากับ 80      ได้ A
+//      ถ้าคะแนน อยู่ระหว่าง 70 - 79    ได้ B
+//      ถ้าคะแนน อยู่ระหว่าง 60 - 69    ได้ C
+//      ถ้าคะแนน อยู่ระหว่าง 50 - 59    ได้ D
+//      ถ้าคะแนน น้อยกว่า 50           ได้ F
+
+let score = prompt("กรุณาระบุคแนน");
+
+if (score >= 80) {
+  alert("Grade A");
+} else if (score >= 70) {
+  alert("Grade B");
+} else if (score >= 60) {
+  alert("Grade C");
+} else if (score >= 50) {
+  alert("Grade D");
+} else {
+  alert("Grade F");
+}
+
+// score >= 80
+//   ? alert("Grade A")
+//   : score >= 70
+//   ? alert("Grade B")
+//   : score >= 60
+//   ? alert("Grade C")
+//   : score >= 50
+//   ? alert("Grade D")
+//   : alert("Grade F");
