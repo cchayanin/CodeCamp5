2fb4cd67349264eff618028214b9fd93a9ac7412
cchay <cchayanin@gmail.com>
2020-03-25 16:18:22 +0700

    improved code

--- 5.Javascript/exercise7/exercise7.6.js
+++ 5.Javascript/exercise7/exercise7.6.js
@@ -3,32 +3,29 @@
 // และให้ผู้เล่นคนที่สองทายเลขโดยการพิมพ์เลขใส่ใน prompt จนกว่าจะถูก ถ้าไม่ถูก จะต้องบอกด้วยว่าเลขที่ผู้เล่นคนที่สองพิมพ์เข้ามา มากกว่า หรือ น้อยกว่าคำตอบนั้น
 
 function saveSecret() {
-  let secret = document.getElementById("secret").value;
+  let secret = Number(document.getElementById("secret").value);
   document.getElementById("secret").value = "";
+  document.getElementById("secret").focus();
   if (isNaN(secret)) {
-    document.getElementById("secret").value = "";
-    document.getElementById("secret").focus();
     alert("กรุณากรอกเฉพาะตัวเลข");
-  } else if (secret < 0 || secret > 100) {
-    document.getElementById("secret").value = "";
-    document.getElementById("secret").focus();
+  } else if (secret < 1 || secret > 100) {
     alert("ตัวเลขต้องมีค่าระหว่าง 1 - 100");
   } else {
     let guest;
     while (true) {
-      guest = prompt("ทายตัวเลข");
+      guest = Number(prompt("ทายตัวเลข"));
       if (guest === null) {
         alert("ยกเลิก");
         break;
       } else if (isNaN(guest)) {
         alert("กรุณากรอกเฉพาะตัวเลข");
-      } else if (guest < 0 || guest > 100) {
+      } else if (guest < 1 || guest > 100) {
         alert("ตัวเลขต้องมีค่าระหว่าง 1 - 100");
       } else {
         if (secret === guest) {
           alert("ถูกต้อง");
           break;
-        } else if (Number(secret) < Number(guest)) {
+        } else if (secret < guest) {
           alert("มากกว่า");
         } else {
           alert("น้อยกว่า");
