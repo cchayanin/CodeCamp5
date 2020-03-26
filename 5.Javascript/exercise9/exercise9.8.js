3a9879355aa903e879c10cc835f4604f08c7a27a
cchay <cchayanin@gmail.com>
2020-03-26 21:01:39 +0700

    improved code

--- 5.Javascript/exercise9/exercise9.8.js
+++ 5.Javascript/exercise9/exercise9.8.js
@@ -2,10 +2,8 @@ function draw(n) {
   let star = "";
   let count = 0;
   for (let i = 1; i <= n; i++) {
-    for (let j = 1; j <= 1; j++) {
-      star += String(count);
-      count += 2;
-    }
+    star += String(count);
+    count += 2;
     star += "\n";
   }
   console.log(star);
