09b702ad9d4d30e069ac0c747e743949c29d79b2
cchay <cchayanin@gmail.com>
2020-03-27 13:06:29 +0700

    removed code in comment

--- 5.Javascript/exercise9/exercise9.17.js
+++ 5.Javascript/exercise9/exercise9.17.js
@@ -7,17 +7,6 @@ function draw(n) {
     star += "\n";
   }
   console.log(star);
-  // let star = "";
-  // for (let i = 1; i <= n; i++) {
-  //   for (let j = 1; j <= n - i; j++) {
-  //     star += "-";
-  //   }
-  //   for (let k = 1; k <= i; k++) {
-  //     star += "*";
-  //   }
-  //   star += "\n";
-  // }
-  // console.log(star);
 }
 
 draw(2);
