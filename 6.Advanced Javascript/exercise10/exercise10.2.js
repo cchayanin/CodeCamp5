cf928665643dffdd17cad5fc6c682bc4d2d5188b
cchay <cchayanin@gmail.com>
2020-04-11 20:39:53 +0700

    fixed wrong condition in exercise10.2.12

--- 6.Advanced Javascript/exercise10/exercise10.2.js
+++ 6.Advanced Javascript/exercise10/exercise10.2.js
@@ -2,21 +2,21 @@
 
 // 2.1  array1 = [1, 2, 30, 400]
 //      array2 [30, 400] // filter เลขที่มากกว่า 10
-let array1 = [1, 2, 30, 400].filter(x => {
+let array1 = [1, 2, 30, 400].filter((x) => {
   return x > 10;
 });
 console.log(array1);
 //------------------------------------------------------------------------------
 // 2.2  array1 = [1, 2, 3, 4]
 //      array2 [1, 3] // filter เลขคี่
-let array2 = [1, 2, 3, 4].filter(x => {
+let array2 = [1, 2, 3, 4].filter((x) => {
   return x % 2 == 1;
 });
 console.log(array2);
 //------------------------------------------------------------------------------
 // 2.3  array1 = [1, "1", 2, {}]
 //      array2 [1, 2] // filter Number
-let array3 = [1, "1", 2, {}].filter(x => {
+let array3 = [1, "1", 2, {}].filter((x) => {
   return typeof x === "number";
 });
 console.log(array3);
@@ -24,7 +24,7 @@ console.log(array3);
 // 2.4  array1 = ["apple", "banana", "orange", "pineapple", "watermeon"]
 //      array2 ["pineapple", "watermeon"] // filter ตัวอักษร > 6
 let array4 = ["apple", "banana", "orange", "pineapple", "watermeon"].filter(
-  x => {
+  (x) => {
     return x.length > 6;
   }
 );
@@ -46,8 +46,8 @@ let array5 = [
   { name: "banana", age: 18 },
   { name: "watermelon", age: 32 },
   { name: "pineapple", age: 16 },
-  { name: "peach", age: 24 }
-].filter(x => {
+  { name: "peach", age: 24 },
+].filter((x) => {
   return x.age < 18;
 });
 console.log(array5);
@@ -70,36 +70,36 @@ let array6 = [
   { name: "banana", age: 18 },
   { name: "watermelon", age: 32 },
   { name: "pineapple", age: 16 },
-  { name: "peach", age: 24 }
-].filter(x => {
+  { name: "peach", age: 24 },
+].filter((x) => {
   return x.age != 32;
 });
 console.log(array6);
 //------------------------------------------------------------------------------
 // 2.7  array1 = [1, -3, 2, 8, -4, 5]
 //      array2 [1, 2, 8, 5] // filter เลขบวก
-let array7 = [1, -3, 2, 8, -4, 5].filter(x => {
+let array7 = [1, -3, 2, 8, -4, 5].filter((x) => {
   return x > 0;
 });
 console.log(array7);
 //------------------------------------------------------------------------------
 // 2.8  array1 = [1,3,4,5,6,7,8]
 //      array2 [3, 6] // filter เลขหาร 3 ลงตัว
-let array8 = [1, 3, 4, 5, 6, 7, 8].filter(x => {
+let array8 = [1, 3, 4, 5, 6, 7, 8].filter((x) => {
   return x % 3 == 0;
 });
 console.log(array8);
 //------------------------------------------------------------------------------
 // 2.9  array1 = ["peach", 1, -3, "2", {}, []]
 //      array2 ["peach", "2"] // filter string
-let array9 = ["peach", 1, -3, "2", {}, []].filter(x => {
+let array9 = ["peach", 1, -3, "2", {}, []].filter((x) => {
   return typeof x == "string";
 });
 console.log(array9);
 //------------------------------------------------------------------------------
 // 2.10 array1 = ["APPLE", "appLE", "PEACH", "PEach"]
 //      array2 = ["APPLE", "PEACH"] // filter คำที่เป็นอักษรใหญ่ทุกตัว
-let array10 = ["APPLE", "appLE", "PEACH", "PEach"].filter(x => {
+let array10 = ["APPLE", "appLE", "PEACH", "PEach"].filter((x) => {
   return x === x.toUpperCase();
 });
 console.log(array10);
@@ -118,8 +118,8 @@ let array11 = [
   { name: "apple", birth: "2001-01-01" },
   { name: "banana", birth: "1990-10-10" },
   { name: "watermelon", birth: "1985-12-30" },
-  { name: "peach", birth: "2002-10-13" }
-].filter(x => {
+  { name: "peach", birth: "2002-10-13" },
+].filter((x) => {
   return new Date(x.birth).getMonth() == 9;
 });
 console.log(array11);
@@ -138,8 +138,8 @@ let array12 = [
   { name: "apple", birth: "2001-01-01" },
   { name: "banana", birth: "1990-10-10" },
   { name: "watermelon", birth: "1985-12-30" },
-  { name: "peach", birth: "2002-10-13" }
-].filter(x => {
-  return new Date(x.birth).getFullYear() <= 2000;
+  { name: "peach", birth: "2002-10-13" },
+].filter((x) => {
+  return new Date(x.birth).getFullYear() < 2000;
 });
 console.log(array12);
