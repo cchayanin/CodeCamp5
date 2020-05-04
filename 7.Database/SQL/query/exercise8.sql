4d6a3450e49ddd55baa691e6a7e8b19c7c6f71fc
cchay <cchayanin@gmail.com>
2020-05-04 16:49:08 +0700

    fixed query exercise8

--- 7.Database/SQL/query/exercise8.sql
+++ 7.Database/SQL/query/exercise8.sql
@@ -1,36 +1,15 @@
---1
 SELECT
-  s.sname
+  DISTINCT s.sname
 FROM sailors s
 JOIN reserves r ON s.sid = r.sid
 JOIN boats b ON r.bid = b.bid
 WHERE
-  b.color = "Red"
-UNION
-SELECT
-  s.sname
-FROM sailors s
-JOIN reserves r ON s.sid = r.sid
-JOIN boats b ON r.bid = b.bid
-WHERE
-  b.color = "Green";
---2
-SELECT
-  sname
-FROM (
+  color = "Red"
+  AND r.sid IN (
     SELECT
-      s.sname
-    FROM sailors s
-    JOIN reserves r ON s.sid = r.sid
-    JOIN boats b ON r.bid = b.bid
-    WHERE
-      b.color = "Red"
-    UNION
-    SELECT
-      s.sname
-    FROM sailors s
-    JOIN reserves r ON s.sid = r.sid
+      r.sid
+    FROM reserves r
     JOIN boats b ON r.bid = b.bid
     WHERE
       b.color = "Green"
-  );
+  )
