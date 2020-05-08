20f441e9337739e4e6b4194912a48329617db593
cchay <cchayanin@gmail.com>
2020-05-08 20:44:34 +0700

    callback

---
+++ 8.Nodejs/callback/lab1.js
@@ -0,0 +1,12 @@
+setTimeout(() => {
+  console.log("A");
+  setTimeout(() => {
+    console.log("B");
+    setTimeout(() => {
+      console.log("C");
+      setTimeout(() => {
+        console.log("D");
+      }, 1000);
+    }, 1000);
+  }, 1000);
+}, 1000);
