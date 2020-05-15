0bc4a69c90242d588529ec6f769c61b14ef3e90f
cchay <cchayanin@gmail.com>
2020-05-16 01:45:35 +0700

    added controller exerice lab1

---
+++ 8.Nodejs/exercise/lab1/app.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const app = express();
+
+const PORT = 3000;
+
+app.get("/static", (req, res) => {
+  res.send("Hello World");
+});
+
+app.get("/staticJSON", (req, res) => {
+  let jsonObj = { text: "Hello World" };
+  res.send(jsonObj);
+});
+
+app.get("/echo", (req, res) => {
+  res.send(req.query.text);
+});
+
+app.get("/plus", (req, res) => {
+  let a = parseInt(req.query.a);
+  let b = parseInt(req.query.b);
+  res.send(`${a + b}`);
+});
+
+app.get("/plusbyjson", (req, res) => {
+  let jsonObj = JSON.parse(req.query.jsonText);
+  res.send(`${jsonObj.a + jsonObj.b}`);
+});
+
+app.get("/plus/:a/:b", (req, res) => {
+  let a = parseInt(req.params.a);
+  let b = parseInt(req.params.b);
+  res.send(`${a + b}`);
+});
+
+app.get("/checkEvenNumber/:numberInput", (req, res) => {
+  if (parseInt(req.params.numberInput) % 2 === 0) {
+    res.send(`${true}`);
+  } else {
+    res.send(`${false}`);
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`app listening at http://localhost:${PORT}`);
+});
