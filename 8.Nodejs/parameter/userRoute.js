d8cb993439c883e650de319b951a6921a8423653
cchay <cchayanin@gmail.com>
2020-05-16 15:05:38 +0700

    fixed variable declaration & converted req.params to int

--- 8.Nodejs/parameter/userRoute.js
+++ 8.Nodejs/parameter/userRoute.js
@@ -1,10 +1,10 @@
-let express = require("express");
-let router = express.Router();
+const express = require("express");
+const router = express.Router();
 
 router.get("/add/:a/:b", function (req, res) {
-  let a = req.params.a;
-  let b = req.params.b;
-  res.send(a + b);
+  let a = parseInt(req.params.a);
+  let b = parseInt(req.params.b);
+  res.send(`${a + b}`);
 });
 
 module.exports = router;
